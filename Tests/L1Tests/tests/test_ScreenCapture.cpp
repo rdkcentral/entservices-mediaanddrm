e937658e7e2536e9924bb1c5fb3a1eb12f5d7536
karuppaiya kaliyaperumal <kkaliyaperumal@synamedia.com>
2025-07-14 12:08:26 +0530

    fix for return value check

--- Tests/L1Tests/tests/test_ScreenCapture.cpp
+++ Tests/L1Tests/tests/test_ScreenCapture.cpp
@@ -140,7 +140,7 @@ TEST_F(ScreenCaptureTest, RegisteredMethods)
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("uploadScreenCapture")));
 }
 
-#if 0
+#if 1
 TEST_F(ScreenCaptureDRMTest, Upload)
 {   
     DRMScreenCapture drmHandle = {0, 1280, 720, 5120, 32};
@@ -212,14 +212,14 @@ TEST_F(ScreenCaptureDRMTest, Upload)
         close(connection);
     });
 
-    EVENT_SUBSCRIBE(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message);
+    EXPECT_EQ(Core::ERROR_NONE, EVENT_SUBSCRIBE(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message));
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("uploadScreenCapture"), _T("{\"url\":\"http://127.0.0.1:11111\"}"), response));
     EXPECT_EQ(response, _T("{\"success\":true}"));
 
     EXPECT_EQ(Core::ERROR_NONE, uploadComplete.Lock());
 
-    EVENT_UNSUBSCRIBE(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message);
+    EXPECT_EQ(Core::ERROR_NONE, EVENT_UNSUBSCRIBE(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message));
 
     free(buffer);
     thread.join();
