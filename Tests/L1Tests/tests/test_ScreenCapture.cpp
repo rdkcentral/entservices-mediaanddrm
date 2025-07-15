18c232027e2b396043a4168d80108220d801c56e
karuppaiya kaliyaperumal <kkaliyaperumal@synamedia.com>
2025-07-15 11:22:03 +0530

    local macro

--- Tests/L1Tests/tests/test_ScreenCapture.cpp
+++ Tests/L1Tests/tests/test_ScreenCapture.cpp
@@ -140,6 +140,24 @@ TEST_F(ScreenCaptureTest, RegisteredMethods)
     EXPECT_EQ(Core::ERROR_NONE, handler.Exists(_T("uploadScreenCapture")));
 }
 
+//#define EVENT_SUBSCRIBE_1(__A__, __B__, __C__, __D__)   { plugin->Subscribe(__A__, __B__, __C__); (void)__D__; }
+//#define EVENT_UNSUBSCRIBE_1(__A__, __B__, __C__, __D__) { plugin->Unsubscribe(__A__, __B__, __C__); (void)__D__; }
+
+#define EVENT_SUBSCRIBE_1(__A__, __B__, __C__, __D__) \
+    { \
+        auto ret = plugin->Subscribe(__A__, __B__, __C__); \
+        EXPECT_EQ(Core::ERROR_NONE, ret); \
+        (void)__D__; \
+    }
+
+#define EVENT_UNSUBSCRIBE_1(__A__, __B__, __C__, __D__) \
+    { \
+        auto ret = plugin->Unsubscribe(__A__, __B__, __C__); \
+        EXPECT_EQ(Core::ERROR_NONE, ret); \
+        (void)__D__; \
+    }
+
+
 #if 1
 TEST_F(ScreenCaptureDRMTest, Upload)
 {   
@@ -212,14 +230,14 @@ TEST_F(ScreenCaptureDRMTest, Upload)
         close(connection);
     });
 
-    EXPECT_EQ(Core::ERROR_NONE, EVENT_SUBSCRIBE(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message));
+    EVENT_SUBSCRIBE_1(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message);
 
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("uploadScreenCapture"), _T("{\"url\":\"http://127.0.0.1:11111\"}"), response));
     EXPECT_EQ(response, _T("{\"success\":true}"));
 
     EXPECT_EQ(Core::ERROR_NONE, uploadComplete.Lock());
 
-    EXPECT_EQ(Core::ERROR_NONE, EVENT_UNSUBSCRIBE(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message));
+    EVENT_UNSUBSCRIBE_1(0, _T("uploadComplete"), _T("org.rdk.ScreenCapture"), message);
 
     free(buffer);
     thread.join();
