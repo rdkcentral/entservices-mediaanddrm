d1c4ae9902cbaa4cc03a02ff6510f2fbed5f786c
kkarup694 <kumar_Karuppiahyogarajah@comcast.com>
2025-12-12 05:38:03 +0000

    L2_test

--- TextToSpeech/TextToSpeechImplementation.cpp
+++ TextToSpeech/TextToSpeechImplementation.cpp
@@ -551,6 +551,7 @@ namespace Plugin {
 
     }
 
+    /* l2 test*/
     void TextToSpeechImplementation::onTTSStateChanged(bool state)
     {
         TTSLOG_INFO("Notify onttsstatechanged, state: %s", (state ? "true" : "false"));
