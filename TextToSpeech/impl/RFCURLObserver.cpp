78566318f2068473fc1fd2f32acd3bba5c94424d
kkarup694 <kumar_Karuppiahyogarajah@comcast.com>
2026-03-12 19:29:15 +0000

    RDKEMW-60642 : improve TTS  testcases
    Reason for change: testcase added using copilot
    Test Procedure: Mentioned in ticket
    Risks: Low
    version: minor
    
    Signed-off-by: kumar <kumar_Karuppiahyogarajah@comcast.com>

--- TextToSpeech/impl/RFCURLObserver.cpp
+++ TextToSpeech/impl/RFCURLObserver.cpp
@@ -121,12 +121,13 @@ string RFCURLObserver::getSecurityToken() {
 void RFCURLObserver::registerNotification() {
     if (m_systemService == nullptr && !m_eventRegistered) {
         std::string token = getSecurityToken();
+#ifndef UNIT_TESTING
         if(token.empty()) {
             m_systemService = new WPEFramework::JSONRPC::LinkType<Core::JSON::IElement>(_T(SYSTEMSERVICE_CALLSIGN_VER),"");
         } else {        
             m_systemService = new WPEFramework::JSONRPC::LinkType<Core::JSON::IElement>(_T(SYSTEMSERVICE_CALLSIGN_VER),"", false, token);
         }
-
+         TTSLOG_INFO("Subscribing to onDeviceMgtUpdateReceived notification");
         while (!m_eventRegistered) {		
             if (m_systemService->Subscribe<JsonObject>(3000, "onDeviceMgtUpdateReceived",
                 &RFCURLObserver::onDeviceMgtUpdateReceivedHandler, this) == Core::ERROR_NONE) {
@@ -139,6 +140,7 @@ void RFCURLObserver::registerNotification() {
             }
         }
     }
+#endif
 }
 
 void RFCURLObserver::onDeviceMgtUpdateReceivedHandler(const JsonObject& parameters) {
