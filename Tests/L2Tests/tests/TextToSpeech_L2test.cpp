16434078c19c95945aa9d226cc28da3f24c9c698
kkarup694 <kumar_Karuppiahyogarajah@comcast.com>
2026-06-11 09:43:55 +0000

    RDKEMW-60642: improve TTS testcases
    Reason for change: adding L1 using copilot
    Test Procedure: Mentioned in ticket
    Risks: Low
    version: minor
    
    Signed-off-by: kumar <kumar_Karuppiahyogarajah@comcast.com>

--- Tests/L2Tests/tests/TextToSpeech_L2test.cpp
+++ Tests/L2Tests/tests/TextToSpeech_L2test.cpp
@@ -33,7 +33,7 @@
 
 #define TTS_CONFIG_FILE_PATH "/etc/entservices/ttsConfig.json"
 
-#define JSON_TIMEOUT (5000)
+#define JSON_TIMEOUT (10000)
 #define TEST_LOG(x, ...)                                                                                                                         \
     fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); \
     fflush(stderr);
