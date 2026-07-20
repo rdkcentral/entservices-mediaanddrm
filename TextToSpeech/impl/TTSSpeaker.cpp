1186f4dfa08ee0e4b4b89d37c4c1fdfd47b09d26
kkarup694 <kumar_Karuppiahyogarajah@comcast.com>
2026-07-20 12:06:55 +0000

    change to test with dummy server

--- TextToSpeech/impl/TTSSpeaker.cpp
+++ TextToSpeech/impl/TTSSpeaker.cpp
@@ -871,7 +871,8 @@ std::string TTSSpeaker::constructURL(TTSConfiguration &config, SpeechData &d) {
 
 void TTSSpeaker::play(string url, SpeechData &data, bool authrequired, string token) {
     m_currentSpeech = &data;
-    g_object_set(G_OBJECT(m_source), "location", url.c_str(), NULL);
+    //g_object_set(G_OBJECT(m_source), "location", url.c_str(), NULL);
+    g_object_set(G_OBJECT(m_source), "location", "http://10.0.0.202:8000/large.mp3", NULL);
     if(authrequired)
     {
         string authStr = "test, Authorization=(string)\"Bearer\\ " + token + "\"";
